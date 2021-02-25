e6f598f6e5cc412ae02c7123a0829640a9337edb
Harish S.G <harishsg99@gmail.com>
2021-02-25 17:30:01 +0530

    updated working key!!!

--- config/config.js
+++ config/config.js
@@ -3,14 +3,14 @@ const env = process.env.NODE_ENV || 'production'
 //insert your API Key & Secret for each environment, keep this file local and never push it to a public repo for security purposes.
 const config = {
 	development :{
-		APIKey : 'REPLACE_WITH_ZOOM_API_KEY',
-		APISecret : 'REPLACE_WITH_ZOOM_API_SECRET',
+		APIKey : 'KMHxZWB2TMu8ttFQTOpbow',
+		APISecret : 'KsyQQfSogWK49pdtM7aCrrHDZDWmBI72geuH',
         	accountSid : 'AC9d76dd023b4bab63cc62ea0326176b21',
         	authToken : 'df0a8d46bb4dc3e6f931f040f9fb2303'
 	},
 	production:{	
-		APIKey : 'REPLACE_WITH_ZOOM_API_KEY',
-		APISecret : 'REPLACE_WITH_ZOOM_API_SECRET',
+		APIKey : 'KMHxZWB2TMu8ttFQTOpbow',
+		APISecret : 'KsyQQfSogWK49pdtM7aCrrHDZDWmBI72geuH',
        	        accountSid : 'AC9d76dd023b4bab63cc62ea0326176b21',
         	authToken : 'df0a8d46bb4dc3e6f931f040f9fb2303'
 	}
