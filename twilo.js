26c239b20a9231975dd77ade398a0f2ec7fae262
Harish S.G <harishsg99@gmail.com>
2021-02-25 17:36:11 +0530

    Update twilo.js

--- twilo.js
+++ twilo.js
@@ -1,7 +1,7 @@
 const client = require('twilio')(accountSid, authToken);
 const zoom = require('./config'); /// for getting MSG wit url
 
-function send(meetingURL) {
+module.exports = function send(meetingURL) {
   client.messages.create({
     from: 'whatsapp:+14155238886',
     body: "Don't forget that you have a Zoom meeting to attend in 5 minutes! Find the meeting link here: "+meetingURL,
@@ -9,4 +9,4 @@ function send(meetingURL) {
   }).then(message => console.log(message.sid));
 }
 
-module.exports = send;
+
