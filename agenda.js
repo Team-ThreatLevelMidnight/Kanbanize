5b6c16716d47fef70239f8d228381fcdf21edb34
Harish S.G <harishsg99@gmail.com>
2021-02-25 17:00:45 +0530

    Update agenda.js

--- agenda.js
+++ agenda.js
@@ -1,10 +1,13 @@
 const agenda = new Agenda();
 const twilo=require("./twilo");
+const sentmail=require("./sendMail");
+
 
 function scheduleMsg(start_url) {
   await agenda.start();
   agenda.define("Notify users", async (job) => {
     await twilo.send(start_url);
+    await sentmail.sendEmail(receiver_email,email_subject,email_body);
   });
   
   (async function () {
