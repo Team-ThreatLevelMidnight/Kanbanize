e222a528e9ee3359abcd467f0927d8364d21389b
aaditya47 <50749476+aaditya47@users.noreply.github.com>
2021-02-25 20:46:06 +0530

    Update agenda.js

--- agenda.js
+++ agenda.js
@@ -5,9 +5,9 @@ const sentmail=require("./sendMail");
 
 module.exports = function scheduleMsg(start_url,time) {
   // Code for parsing date and time goes here
-  temp=time.substring(1,time.length-1);
-  temp2=temp.split('-');
-  datestr=temp2[0].split('/');
+  temp=time.substring(0,time.length-1);
+  temp2=temp.split('T');
+  datestr=temp2[0].split('-');
   timestr=temp2[1].split(':');
   const date=new Date(parseInt(datestr[0]),parseInt(datestr[1]),parseInt(datestr[2]),parseInt(timestr[0]),parseInt(timestr[1]),parseInt(timestr[2]));
   const job=schedule.scheduleJob(date,function() {
@@ -15,7 +15,3 @@ module.exports = function scheduleMsg(start_url,time) {
     sentmail.sendEmail(receiver_email,email_subject,email_body);
   });
 }
-
-/*
-If there is a problem, we need to modify Line 8 to Line 12
-*/
