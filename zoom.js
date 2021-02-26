4118133279311ee2f4269751bb518a8fd676ddd5
Harish S.G <harishsg99@gmail.com>
2021-02-26 13:20:47 +0530

    Add files via upload

--- zoom.js
+++ zoom.js
@@ -1,25 +1,11 @@
-const config = require('./config/config');
-const agenda=require("./agenda");
-const User = require("./models/UserSchema");
+const config = require('./config');
 
-function test()
-{
-    console.log("Test");
-}
-
-async function StartZoomMeeting(topicname,date) {
-    const email=User.find({
-        "email":email
-    },(err,teams) => {
-        console.log(err);
-    });
-    console.log(email);
+async function StartZoomMeeting() {
     const options = {
         method: "POST",
         uri: "https://api.zoom.us/v2/users/" + email + "/meetings",
         body: {
-            topic: topicname,
-            start_time: date,
+            topic: "test create meeting",
             type: 1,
             settings: {
                 host_video: "true",
@@ -28,7 +14,7 @@ async function StartZoomMeeting(topicname,date) {
             }
         },
         auth: {
-            bearer:"eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhdWQiOm51bGwsImlzcyI6IktNSHhaV0IyVE11OHR0RlFUT3Bib3ciLCJleHAiOjE2MTQ5MjQ5NTMsImlhdCI6MTYxNDMyMDE2OH0.jLl7V1bU97R1NvDAzqbMermO3zBH8QjK5rHnU-1mvas"
+            bearer: token
         },
         headers: {
             "User-Agent": "Zoom-api-Jwt-Request",
@@ -42,12 +28,10 @@ async function StartZoomMeeting(topicname,date) {
         result = JSON.stringify(response);
         result = JSON.parse(result);
         console.log(result.start_url);
-        agenda.scheduleMsg(result.start_url,result.start_time); // To schedule msg send
+        opn(result.start_url);
 
 
     } catch (error) {
         console.log("API call failed, reason ", error);
     }
 }
-
-module.exports.StartZoomMeeting=StartZoomMeeting;
