0ebc3d00f691e94a670c7d3ce4c563338d2a10bd
Harish S.G <harishsg99@gmail.com>
2021-02-27 16:17:13 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const passport = require("passport");
 const path = require("path");
-
+const cors = require("cors");
 const users = require("./routes/api/users");
 const dashboard = require("./routes/dashboard");
 
