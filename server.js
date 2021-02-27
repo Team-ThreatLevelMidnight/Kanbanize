5e12226e47ded2181596ccb9b4b1c5c02c978066
aaditya47 <50749476+aaditya47@users.noreply.github.com>
2021-02-27 16:36:23 +0530

    Update server.js

--- server.js
+++ server.js
@@ -34,6 +34,26 @@ app.use(passport.initialize());
 // Passport config
 require("./config/passport")(passport);
 
+// Add headers
+app.use(function (req, res, next) {
+
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8888/%27);
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  // Pass to next layer of middleware
+  next();
+});
+
 // Routes
 app.use("/api/users", users);
 app.use("/dashboard", dashboard);
