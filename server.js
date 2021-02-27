8480a8736a8a0213811bf40e29740be63c41ef6a
Harish S.G <harishsg99@gmail.com>
2021-02-27 16:42:41 +0530

    Update server.js

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ require("./config/passport")(passport);
 app.use(function (req, res, next) {
 
   // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8888/%27);
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000/');
 
   // Request methods you wish to allow
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
