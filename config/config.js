a1953c254dfe012f82a8cb3dc14528a3ad8e44df
Harish S.G <harishsg99@gmail.com>
2021-02-28 15:43:02 +0530

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,21 +1,21 @@
 const env = process.env.NODE_ENV || 'production'
 
-const accountSid = 'AC9d76dd023b4bab63cc62ea0326176b21';
-const authToken = 'df0a8d46bb4dc3e6f931f040f9fb2303';
+const accountSid = '';
+const authToken = '';
 
 //insert your API Key & Secret for each environment, keep this file local and never push it to a public repo for security purposes.
 const config = {
 	development :{
-		APIKey : 'KMHxZWB2TMu8ttFQTOpbow',
-		APISecret : 'KsyQQfSogWK49pdtM7aCrrHDZDWmBI72geuH',
-        accountSid : 'AC9d76dd023b4bab63cc62ea0326176b21',
-        authToken : 'df0a8d46bb4dc3e6f931f040f9fb2303'
+		APIKey : '',
+		APISecret : '',
+        accountSid : '',
+        authToken : ''
 	},
 	production:{	
-		APIKey : 'KMHxZWB2TMu8ttFQTOpbow',
-		APISecret : 'KsyQQfSogWK49pdtM7aCrrHDZDWmBI72geuH',
-       	accountSid : 'AC9d76dd023b4bab63cc62ea0326176b21',
-        authToken : 'df0a8d46bb4dc3e6f931f040f9fb2303'
+		APIKey : '',
+		APISecret : '',
+       	accountSid : '',
+        authToken : ''
 	}
 };
 
